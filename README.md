0e0aa48b6e13117fc24e9890fea8edb88746f2da
Craig <craig.treptow@gmail.com>
2014-07-23 20:45:25 -0500

    added some more baby stuff

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 learn-haskell
 =============
 
+REPL
+====
+
 ghci
 :l baby - loads the baby.hs file
 :q - quits
+
+Compile
+=======
+
+edit somefile.hs
+
+ghc --make somefile
+
+./somefile
+
 
--- baby.hs
+++ baby.hs
@@ -1,2 +1,22 @@
+module Baby where
+
 doubleMe x = x + x
 doubleUs x y = doubleMe x + doubleMe y
+
+main = 
+  doit1
+  doit2
+
+doit1 = 
+  odd_numbers = [ 1, 3, 5, 7, 9 ]
+  even_numbers = [ 2, 4, 6, 8, 10 ]
+  all_numbers = odd_numbers ++ even_numbers
+
+doit2 =
+  letters = [ 'a', 'b', 'c' ]
+  more_letters = [ 'd', 'e', 'f' ]
+
+  letters : more_letters
+
+putStrLn doit1
+putStrLn doit2
